3d675e69919b58c232a0e4fe6567f58046569bc4
MARCUS VINICIUS REIS GONCALVES <marcus.vrgoncalves@senacsp.edu.br>
2026-05-28 11:37:15 -0300

    feat: hello world

--- src/main.c
+++ src/main.c
@@ -1,6 +1,37 @@
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 
-int main(int argc, int **argv){
-    puts("Olá mundo!");
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("Super Mario Bros. 1 Clone", 0, 0, 640, 480, 0);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
